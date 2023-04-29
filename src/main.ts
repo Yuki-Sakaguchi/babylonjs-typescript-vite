ff555599887cc42c69a426f774da8fda5eccd5d6
Yuki Sakaguchi <sakaguchi.ba55.0ne@gmail.com>
2023-04-29 18:34:05 +0900

    update WebARに対応

--- src/main.ts
+++ src/main.ts
@@ -1,7 +1,7 @@
 import "./style.scss";
 import * as BABYLON from "@babylonjs/core";
 
-const main = () => {
+const main = async () => {
   // キャンバスを取得
   const renderCanvas = <HTMLCanvasElement>(
     document.getElementById("renderCanvas")
@@ -14,12 +14,19 @@ const main = () => {
 
     // カメラとライトを設定
     scene.createDefaultCameraOrLight(true, true, true);
-    scene.createDefaultEnvironment();
+    // scene.createDefaultEnvironment();
 
     // 箱を生成
     const boxSize = 0.2;
     const box = BABYLON.MeshBuilder.CreateBox("box", { size: boxSize });
-    box.position.addInPlaceFromFloats(0, boxSize / 2.0, 0);
+    box.position.addInPlaceFromFloats(0, 1.6, 0);
+
+    // WebAR対応
+    await scene.createDefaultXRExperienceAsync({
+      uiOptions: {
+        sessionMode: "immersive-ar",
+      },
+    });
 
     // レンダリング
     engine.runRenderLoop(() => {
