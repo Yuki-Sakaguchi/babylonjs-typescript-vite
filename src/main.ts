274e1c6941133f6809d1c848512dc7f8adbcaf54
Yuki Sakaguchi <sakaguchi.ba55.0ne@gmail.com>
2023-04-29 19:05:21 +0900

    update WebARに対応

--- src/main.ts
+++ src/main.ts
@@ -12,14 +12,27 @@ const main = async () => {
     const engine = new BABYLON.Engine(renderCanvas, true);
     const scene = new BABYLON.Scene(engine);
 
-    // カメラとライトを設定
-    scene.createDefaultCameraOrLight(true, true, true);
-    // scene.createDefaultEnvironment();
+    // カメラを設定
+    const camera = new BABYLON.FreeCamera(
+      "camera",
+      new BABYLON.Vector3(0, 5, -10),
+      scene
+    );
+    camera.setTarget(BABYLON.Vector3.Zero());
+    camera.attachControl(renderCanvas, true);
+
+    // ライトを設定
+    const light = new BABYLON.HemisphericLight(
+      "light",
+      new BABYLON.Vector3(0, 1, 0),
+      scene
+    );
+    light.intensity = 0.7;
 
     // 箱を生成
     const boxSize = 0.2;
     const box = BABYLON.MeshBuilder.CreateBox("box", { size: boxSize });
-    box.position.addInPlaceFromFloats(0, 1.6, 0);
+    box.position.addInPlaceFromFloats(0, 0, 0);
 
     // WebAR対応
     await scene.createDefaultXRExperienceAsync({
